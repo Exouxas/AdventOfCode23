ebfce40da96a8e854f39029d65d3258c5b047148
Exouxas <exouxas@gmail.com>
2023-12-26 18:57:12 +0100

    Solve day 14 part 1

--- AdventOfCode23/Day14/Solver.cs
+++ AdventOfCode23/Day14/Solver.cs
@@ -6,12 +6,43 @@ namespace AdventOfCode23.Day14
     {
         public override string GetPuzzleOutput1()
         {
-            return "ERR";
+            int[] loadCounter = new int[puzzleInput[0].Length];
+            for(int i = 0; i < loadCounter.Length; i++)
+            {
+                loadCounter[i] = Load(0);
+            }
+
+            long sum = 0;
+
+            for(int y = 0; y < puzzleInput.Length; y++)
+            {
+                for(int x = 0; x < puzzleInput[y].Length; x++)
+                {
+                    if (puzzleInput[y][x] == '#')
+                    {
+                        loadCounter[x] = Load(y + 1);
+                    }
+                    else if(puzzleInput[y][x] == 'O')
+                    {
+                        sum += loadCounter[x];
+                        loadCounter[x]--;
+                    }
+                }
+            }
+
+
+
+            return sum.ToString();
         }
 
         public override string GetPuzzleOutput2()
         {
             return "ERR";
+        }
+
+        private int Load(int position)
+        {
+            return puzzleInput.Length - position;
         }
 
         protected override string GetFolderName()
